1bd6955b33744a6e5449a793c0e4b440635a4bf2
Suvash Sedhain <sedhain@adobe.com>
2016-02-11 17:07:17 +1100

    code updated

--- lrec/recommender/LRec/LRec.py
+++ lrec/recommender/LRec/LRec.py
@@ -19,9 +19,9 @@ class LRec(BaseLinear):
 
     def __getLearner(self):
         if self.arg.loss == "squared":
-            return Ridge(alpha=self.l2, fit_intercept=False)
+            return Ridge(alpha=self.l2, fit_intercept=False, max_iter=40)
         elif self.arg.loss == "logistic":
-            return LogisticRegression(C=self.l2, fit_intercept=False)
+            return LogisticRegression(C=self.l2, fit_intercept=False,max_iter=40)
         else:
             raise NotImplementedError(
                 "Model %s not implemented" % (self.arg.loss))
